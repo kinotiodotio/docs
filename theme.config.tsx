1a1dd6bc138f5611905578a4ac96f4253862d715
Ando <hi@andostronaut.com>
2024-10-23 10:54:28 +0300

    feat: struct pages and fix some issue on theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,40 +1,40 @@
-import Link from "next/link";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import Link from 'next/link'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
-import { KinotioLogo } from "@/components/kinotio-logo";
+import { KinotioLogo } from '@/components/kinotio-logo'
 
-import { getCurrentYear } from "@/lib/utils";
+import { getCurrentYear } from '@/lib/utils'
 
-import { DATA } from "@/data";
+import { DATA } from '@/data'
 
 const config: DocsThemeConfig = {
   useNextSeoProps() {
     return {
-      titleTemplate: `%s - ${DATA.name}`,
-    };
+      titleTemplate: `%s - ${DATA.name}`
+    }
   },
   darkMode: true,
   logo: function Logo() {
-    return <KinotioLogo width={100} height={100} />;
+    return <KinotioLogo width={100} height={100} />
   },
   project: {
-    link: DATA.repo,
+    link: DATA.repo
   },
   chat: {
-    link: DATA.social_url.discord,
+    link: DATA.social_url.discord
   },
   docsRepositoryBase: DATA.doc_repo,
   footer: {
-    text: (
+    content: (
       <span>
         {`Copyright © ${getCurrentYear()} Developed by `}
-        <Link href={"https://kinotio.io"} target={"_blank"}>
+        <Link href={'https://kinotio.io'} target={'_blank'}>
           Kinotio
         </Link>
         .
       </span>
-    ),
-  },
-};
+    )
+  }
+}
 
-export default config;
+export default config
