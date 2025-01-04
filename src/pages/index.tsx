e1d4ca5d9a11f8278133962e6b5da33f35dd84f9
Ando <hi@andostronaut.com>
2025-01-04 18:38:27 +0300

    fix: clear theme provider in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Script from 'next/script'
-import { ThemeProvider } from '@/components/theme-provider'
-
 import { DATA } from '@/data'
 
 import { cn } from '@/lib/utils'
@@ -48,35 +46,33 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <ThemeProvider attribute='class' forcedTheme='dark' disableTransitionOnChange>
-      <div className={cn('min-h-screen relative overflow-hidden', [inter.className])}>
-        <Head>
-          <title>{metadata.title.default}</title>
-          <meta name='description' content={metadata.description} />
-          <meta name='keywords' content={metadata.keywords.join(', ')} />
-          <meta property='og:title' content={metadata.openGraph.title} />
-          <meta property='og:description' content={metadata.openGraph.description} />
-          <meta property='og:url' content={metadata.openGraph.url} />
-          <meta property='og:site_name' content={metadata.openGraph.siteName} />
-          <meta property='og:locale' content={metadata.openGraph.locale} />
-          <meta property='og:type' content={metadata.openGraph.type} />
-          <meta name='robots' content='index, follow' />
-          <meta
-            name='googlebot'
-            content='index, follow, max-video-preview:-1, max-image-preview:large, max-snippet:-1'
-          />
-          <meta name='twitter:title' content={metadata.twitter.title} />
-          <meta name='twitter:card' content={metadata.twitter.card} />
-          <meta name='google-site-verification' content={metadata.verification.google} />
-          <meta name='yandex-verification' content={metadata.verification.yandex} />
-        </Head>
-
-        <Script
-          async
-          src='https://umami.kinotio.io/script.js'
-          data-website-id='b74460b2-21dd-46e9-b311-2250008a74bb'
+    <div className={cn('min-h-screen relative overflow-hidden', [inter.className])}>
+      <Head>
+        <title>{metadata.title.default}</title>
+        <meta name='description' content={metadata.description} />
+        <meta name='keywords' content={metadata.keywords.join(', ')} />
+        <meta property='og:title' content={metadata.openGraph.title} />
+        <meta property='og:description' content={metadata.openGraph.description} />
+        <meta property='og:url' content={metadata.openGraph.url} />
+        <meta property='og:site_name' content={metadata.openGraph.siteName} />
+        <meta property='og:locale' content={metadata.openGraph.locale} />
+        <meta property='og:type' content={metadata.openGraph.type} />
+        <meta name='robots' content='index, follow' />
+        <meta
+          name='googlebot'
+          content='index, follow, max-video-preview:-1, max-image-preview:large, max-snippet:-1'
         />
-      </div>
-    </ThemeProvider>
+        <meta name='twitter:title' content={metadata.twitter.title} />
+        <meta name='twitter:card' content={metadata.twitter.card} />
+        <meta name='google-site-verification' content={metadata.verification.google} />
+        <meta name='yandex-verification' content={metadata.verification.yandex} />
+      </Head>
+
+      <Script
+        async
+        src='https://umami.kinotio.io/script.js'
+        data-website-id='b74460b2-21dd-46e9-b311-2250008a74bb'
+      />
+    </div>
   )
 }
