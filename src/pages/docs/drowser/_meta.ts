2acb3f34b7a002179ede6c8b30a4a7c5a3b31aff
Ando <aramanamihanta@gmail.com>
2025-02-10 20:29:47 +0300

    feat: add drowser changelog

--- src/pages/docs/drowser/_meta.ts
+++ src/pages/docs/drowser/_meta.ts
@@ -1,8 +1,10 @@
 const meta = {
-  index: 'Installation',
-  configuration: 'Configuration',
-  usage: 'Usage',
-  structure: 'Structure'
+  'index': 'Installation',
+  'configuration': 'Configuration',
+  'usage': 'Usage',
+  'report-structure': 'Report Structure',
+  'features': 'Features',
+  'changelog': 'Changelog'
 }
 
 export default meta
