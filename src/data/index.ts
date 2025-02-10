c0a72ceebc5abd18675c6c4422db3e53fc5cb109
Ando <aramanamihanta@gmail.com>
2025-02-10 15:51:25 +0300

    feat: update favicon and new folder structure for docs

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,16 +1,17 @@
 export const DATA = {
-  name: 'Kinotio | Docs',
+  name: 'kinotio.io: This platform is dedicated to providing detailed documentation about our products, offering users and developers the necessary insights and guidance to effectively engage with and contribute to our ecosystem.',
   url: 'https://docs.kinotio.io',
   description:
     'This platform is dedicated to providing detailed documentation about our products, offering users and developers the necessary insights and guidance to effectively engage with and contribute to our ecosystem.',
   social_url: {
-    github: 'https://github.com/kinotio',
+    github: 'https://github.com/kinotiodotio',
     discord: 'https://discord.gg/cskvbKQA',
-    linkedin: 'https://linkedin.com/company/kinotio',
-    x: 'https://x.com/kinotiodotio'
+    linkedin: 'https://linkedin.com/company/kinotiodotio',
+    x: 'https://x.com/kinotiodotio',
+    bluesky: 'https://bsky.app/profile/kinotio.io'
   },
-  repo: 'https://github.com/kinotio/docs',
-  doc_repo: 'https://github.com/kinotio/docs/tree/main',
+  repo: 'https://github.com/kinotiodotio/docs',
+  doc_repo: 'https://github.com/kinotiodotio/docs/tree/main',
   keywords: ['Open Source', 'Documentation', 'API', 'Guides', 'Tutorials'],
   color: {
     gradient: {
